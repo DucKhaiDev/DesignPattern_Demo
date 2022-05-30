aa5ff36e17c646a6b59f347fea5662c9b36fe6cd
DucKhaiDev <duckhaidev@gmail.com>
2022-05-31 05:38:07 +0700

    Update

--- src/com/company/SingletonPattern/Dev.java
+++ src/com/company/SingletonPattern/Dev.java
@@ -1,11 +1,27 @@
 package com.company.SingletonPattern;
 
 public class Dev {
-    private Dev(){
+    private String name;
+
+    private Dev() {
+        this.name = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public static Dev getInstance() {
         return SingletonHelper.INSTANCE;
+    }
+
+    @Override
+    public String toString() {
+        return name + " hello!";
     }
 
     private static class SingletonHelper {
--- src/com/company/SingletonPattern/Main.java
+++ src/com/company/SingletonPattern/Main.java
@@ -3,8 +3,11 @@ package com.company.SingletonPattern;
 public class Main {
     public static void main(String[] args) {
         Dev dev_1 = Dev.getInstance();
+        dev_1.setName("Dev 1");
+        System.out.println(dev_1 + " - " + dev_1.hashCode());
+
         Dev dev_2 = Dev.getInstance();
-        System.out.println(dev_1.hashCode());
-        System.out.println(dev_2.hashCode());
+        dev_2.setName("Dev 2");
+        System.out.println(dev_2 + " - " + dev_2.hashCode());
     }
 }
